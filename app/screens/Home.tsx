1506fceec9920a5e2c3510ceb7ca0478615da38b
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2023-08-04 12:11:19 +0530

    added newsitem component on home screen

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,10 +1,10 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View, FlatList} from 'react-native';
+import {StyleSheet, TouchableOpacity, View, FlatList, Text} from 'react-native';
 import axios from 'axios';
 
 import {useTheme} from '../theme/useTheme';
 import Layout from '../components/Layout';
+import NewsItem from '../components/NewsItem'; // Import NewsItem component
 
 const Home = () => {
   const {theme} = useTheme();
@@ -19,12 +19,11 @@ const Home = () => {
 
   useEffect(() => {
     if (categories.length > 0) {
-      handleMainCategoryPress(categories[0]);
+      handleCategoryPress(categories[0]);
     }
   }, [categories]);
 
   useEffect(() => {
-    console.log('fetch post response', selectedCategory, page);
     if (selectedCategory) {
       fetchPostsForCategory(selectedCategory.category_id, page);
     }
@@ -50,57 +49,47 @@ const Home = () => {
       );
       if (response?.data) {
         setPosts(response.data.posts);
-        console.log('fetch post response', response.data);
       }
     } catch (error) {
       console.error('Error fetching posts:', error);
     }
   };
 
-  const renderMainCategory = ({item}) => {
-    const isSelected = item.category_id === selectedCategory?.category_id;
+  const handleCategoryPress = item => {
+    setSelectedCategory(item);
+  };
+
+  const renderItem = ({item}) => {
     return (
       <TouchableOpacity
-        style={[styles.tab, isSelected && styles.selectedTab]}
-        onPress={() => handleMainCategoryPress(item)}>
-        <Text style={[styles.tabText, isSelected && styles.selectedTabText]}>
+        style={[
+          styles.tab,
+          item.category_id === selectedCategory?.category_id &&
+            styles.selectedTab,
+        ]}
+        onPress={() => handleCategoryPress(item)}>
+        <Text
+          style={[
+            styles.tabText,
+            item.category_id === selectedCategory?.category_id &&
+              styles.selectedTabText,
+          ]}>
           {item.menu}
         </Text>
       </TouchableOpacity>
     );
   };
 
-  const handleMainCategoryPress = item => {
-    if (item.sub_menu_status && item.sub_menu.length > 0) {
-      setSelectedCategory(item);
-      handleSubMenuPress(item.sub_menu[0]); // Select the first submenu by default
-    } else {
-      setSelectedCategory(item);
-      handleSubMenuPress(item); // Select the menu by default
-      console.log('Selected main category:', item.menu);
-    }
-  };
-
-  const renderSubMenu = ({item}) => {
+  const renderPost = ({item}) => {
     return (
-      <TouchableOpacity
-        style={styles.subTab}
-        onPress={() => handleSubMenuPress(item)}>
-        <Text style={styles.subTabText}>{item.menu}</Text>
-      </TouchableOpacity>
-    );
-  };
-
-  const handleSubMenuPress = item => {
-    console.log('Selected submenu:', item.menu);
-    setSelectedCategory(item);
-  };
-
-  const renderPost = ({index}) => {
-    return (
-      <View style={styles.post}>
-        <Text style={styles.postText}>Post {index + 1}</Text>
-      </View>
+      <NewsItem
+        key={item.id}
+        title={item.title}
+        featured_image={item.featured_image}
+        content={item.content}
+        timestamp={item.publish_date}
+        url={item.url}
+      />
     );
   };
 
@@ -108,33 +97,20 @@ const Home = () => {
     <Layout>
       <View
         style={[styles.contentContainer, {backgroundColor: theme.layoutBg}]}>
-        <View style={styles.mainCategoriesContainer}>
-          <FlatList
-            data={categories}
-            renderItem={renderMainCategory}
-            keyExtractor={item => item.category_id.toString()}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-          />
-        </View>
-        <View style={styles.subCategoriesContainer}>
-          <FlatList
-            data={selectedCategory?.sub_menu || []}
-            renderItem={renderSubMenu}
-            keyExtractor={item => item.category_id.toString()}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-          />
-        </View>
-        <View style={styles.postsContainer}>
-          <FlatList
-            data={posts}
-            renderItem={renderPost}
-            keyExtractor={(item, index) => index.toString()}
-            onEndReached={() => setPage(page + 1)}
-            onEndReachedThreshold={0.5}
-          />
-        </View>
+        <FlatList
+          data={categories}
+          renderItem={renderItem}
+          keyExtractor={item => item.category_id.toString()}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+        <FlatList
+          data={posts}
+          renderItem={renderPost}
+          keyExtractor={item => item.id.toString()}
+          onEndReached={() => setPage(page + 1)}
+          onEndReachedThreshold={0.5}
+        />
       </View>
     </Layout>
   );
@@ -144,10 +120,6 @@ const styles = StyleSheet.create({
   contentContainer: {
     flexGrow: 1,
     paddingHorizontal: 12,
-  },
-  mainCategoriesContainer: {
-    marginTop: 16,
-    marginBottom: 8,
   },
   tab: {
     paddingHorizontal: 20,
@@ -166,37 +138,6 @@ const styles = StyleSheet.create({
   },
   selectedTabText: {
     color: '#FFF',
-  },
-  subCategoriesContainer: {
-    marginBottom: 16,
-  },
-  subTab: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    marginHorizontal: 8,
-    borderRadius: 8,
-    backgroundColor: '#f0f0f0',
-  },
-  subTabText: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: '#000',
-  },
-  postsContainer: {
-    marginBottom: 16,
-  },
-  post: {
-    height: 150,
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    marginHorizontal: 8,
-    borderRadius: 8,
-    backgroundColor: '#f0f0f0',
-  },
-  postText: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: '#000',
   },
 });
 
